c86788538f9b7b9ada81399192fb293fcdee4ddc
volkron <volkron@users.noreply.github.com>
2020-07-03 18:39:16 -0400

    Add some login tests.

---
+++ tests/Feature/LoginTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Hash;
+use Tests\TestCase;
+
+class LoginTest extends TestCase
+{
+    /**
+     * Test to ensure the user can view the login form.
+     *
+     * @return void Returns nothing.
+     */
+    public function testLoginView()
+    {
+        $response = $this->get('/login');
+        $response->assertSuccessful();
+        $response->assertViewIs('auth.login');
+    }
+
+    /**
+     * Test to ensure the user can not view the login form when authenticated.
+     *
+     * @return void Returns nothing.
+     */
+    public function testLoginView()
+    {
+        $user = factory(User::class)->make();
+        $response = $this->actingAs($user)->get('/login');
+        $response->assertRedirect('/home');
+    }
+
+    /**
+     * Test to ensure the user can view the login form.
+     *
+     * @return void Returns nothing.
+     */
+    public function testLoginAuthenticationFlow()
+    {
+        $user = factory(User::class)->create([
+            'password' => Hash::make($password = 'i-love-laravel'),
+        ]);
+        $response = $this->post('/login', [
+            'email' => $user->username,
+            'password' => $password,
+        ]);
+        $response->assertRedirect('/home');
+        $this->assertAuthenticatedAs($user);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+

10a9bc173f8d9557941f52e2cae23bc816961888
volkron <volkron@users.noreply.github.com>
2020-07-03 23:42:27 -0400

    Add more tests and assertions.

--- tests/Feature/LoginTest.php
+++ tests/Feature/LoginTest.php
@@ -14,6 +14,16 @@ class LoginTest extends TestCase
     use RefreshDatabase;
 
     /**
+     * Formats the too many login attempts message based the passed seconds.
+     *
+     * @return string Returns the too many login attempts message.
+     */
+    protected function getTooManyLoginAttemptsMessage()
+    {
+        return sprintf('/^%s$/', str_replace('\:seconds', '\d+', preg_quote(__('auth.throttle'), '/')));
+    }
+
+    /**
      * Test to ensure the user can view the login form.
      *
      * @return void Returns nothing.
@@ -53,6 +63,128 @@ class LoginTest extends TestCase
         ]);
         $response->assertRedirect('/home');
         $this->assertAuthenticatedAs($user);
+    }
+
+    /**
+     * Test to ensure the user can not login with wrong credentials.
+     *
+     * @return void Returns nothing.
+     */
+    public function testLoginWithWrongCreds()
+    {
+        $user = factory(User::class)->create([
+            'password' => Hash::make($password = 'i-love-laravel'),
+        ]);
+        $response = $this->from('/login')->post('/login', [
+            'username' => $user->username,
+            'password' => 'invalid-password',
+        ]);
+        $response->assertRedirect('/login');
+        $response->assertSessionHasErrors('username');
+        $this->assertTrue(session()->hasOldInput('username'));
+        $this->assertFalse(session()->hasOldInput('password'));
+        $this->assertGuest();
+    }
+
+    /**
+     * Test to ensure the remember me functionality works.
+     *
+     * @return void Returns nothing.
+     */
+    public function testRememberMeFunctionality()
+    {
+        $user = factory(User::class)->create([
+            'password' => Hash::make($password = 'i-love-laravel'),
+        ]);
+        $response = $this->post('/login', [
+            'username' => $user->username,
+            'password' => $password,
+            'remember' => 'on',
+        ]);
+        $response->assertRedirect('/home');
+        $response->assertCookie(Auth::guard()->getRecallerName(), vsprintf('%s|%s|%s', [
+            $user->id,
+            $user->getRememberToken(),
+            $user->password,
+        ]));
+        $this->assertAuthenticatedAs($user);
+    }
+
+    /**
+     * Test to ensure the user can not login using a username that does not exists.
+     *
+     * @return void Returns nothing.
+     */
+    public function testUserCannotLoginWithUsernameThatDoesNotExist()
+    {
+        $response = $this->from('/login')->post('/login', [
+            'username' => 'nobodySpecialHere',
+            'password' => 'invalid-password',
+        ]);
+        $response->assertRedirect('/login');
+        $response->assertSessionHasErrors('username');
+        $this->assertTrue(session()->hasOldInput('username'));
+        $this->assertFalse(session()->hasOldInput('password'));
+        $this->assertGuest();
+    }
+
+    /**
+     * Test to ensure the user can logout.
+     *
+     * @return void Returns nothing.
+     */
+    public function testUserCanLogout()
+    {
+        $this->be(factory(User::class)->create());
+        $response = $this->post('/logout');
+        $response->assertRedirect('/');
+        $this->assertGuest();
+    }
+
+    /**
+     * Test to ensure the user can not logout while not authenticated.
+     *
+     * @return void Returns nothing.
+     */
+    public function testUserCannotLogoutWhenNotAuthenticated()
+    {
+        $response = $this->post($this->logoutRoute());
+        $response->assertRedirect('/');
+        $this->assertGuest();
+    }
+
+    /**
+     * Test to ensure the brute force system is working.
+     *
+     * @return void Returns nothing.
+     */
+    public function testUserCannotMakeMoreThanFiveAttemptsInOneMinute()
+    {
+        $user = factory(User::class)->create([
+            'password' => Hash::make($password = 'i-love-laravel'),
+        ]);
+        foreach (range(0, 5) as $_) {
+            $response = $this->from('/login')->post('/login', [
+                'username' => $user->username,
+                'password' => 'invalid-password',
+            ]);
+        }
+        $response->assertRedirect('/login');
+        $response->assertSessionHasErrors('username');
+        $this->assertRegExp(
+            $this->getTooManyLoginAttemptsMessage(),
+            collect(
+                $response
+                ->baseResponse
+                ->getSession()
+                ->get('errors')
+                ->getBag('default')
+                ->get('email')
+            )->first()
+        );
+        $this->assertTrue(session()->hasOldInput('username'));
+        $this->assertFalse(session()->hasOldInput('password'));
+        $this->assertGuest();
     }
 }
 
